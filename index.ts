a49b135950bf7893a1e8226501df0c74515a39fd
oussamabenalisst <benalio248@gmail.com>
2025-06-29 20:52:07 +0100

    Add mongoose as a dependency and update package files

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
-
+import mongoose, { Schema, Document } from 'mongoose';
 const app = express();
+mongoose.connect("mongodb://localhost:27017/shop").then(()=>{
+  console.log("Error DB Connection")
+})
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
