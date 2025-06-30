b7133ce72e6f3a3df6b5581f2eae4bcfbf1b9084
oussamabenalisst <benalio248@gmail.com>
2025-06-30 15:15:46 +0100

    Refactor product addition logic and improve error handling

--- index.ts
+++ index.ts
@@ -1,37 +1,42 @@
-import express, { Request, Response } from 'express';
-const Product=require("./src/models/products")
-import mongoose, { Schema, Document } from 'mongoose';
+import express, { Request, Response } from "express";
+const Product = require("./src/models/products");
+import mongoose, { Schema, Document } from "mongoose";
 const app = express();
-mongoose.connect("mongodb://localhost:27017/shop").then(()=>{
-  console.log("Connection To Mongo")
-}).catch(()=>{
-  console.log("Error DB Connection")
-})
+app.use(express.json());
+mongoose
+  .connect("mongodb://localhost:27017/shop")
+  .then(() => {
+    console.log("Connection To Mongo");
+  })
+  .catch(() => {
+    console.log("Error DB Connection");
+  });
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Express + TypeScript + mongo!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from Express + TypeScript + mongo!");
 });
 
-app.post('/Addproduct', (req: Request, res: Response) => {
-  var Newproduct = new Product()
-  // const { name, class:ClassP, pr, img } = req.body;
-  // if (
-  //   !name ||
-  //   !ClassP ||
-  //   !pr ||
-  //   !img 
-
-  // ) {
-  //   res.send("error input");
-  // }
-  Newproduct.name = "name";
-  Newproduct.class = "ClassP";
-  Newproduct.pr = 5;
-  Newproduct.img = "img";
-  Newproduct.save().then(() => {
-    res.send('products');
-  })
+app.post("/Addproduct", (req: Request, res: Response) => {
+  try {
+    if (!req.body) {
+      res.send("error req.body");
+    }
+    var Newproduct = new Product();
+    const { name: nameP, class: ClassP, pr, img } = req.body;
+    if (!nameP || !ClassP || !pr || !img) {
+      res.send("error input");
+    }
+    Newproduct.name =  nameP ;
+    Newproduct.class = ClassP;
+    Newproduct.pr = pr;
+    Newproduct.img = img;
+    Newproduct.save().then(() => {
+      res.send("products");
+    });
+  } catch {
+    res.send("Error of catch");
+  }
 });
 
 app.listen(port, () => {
